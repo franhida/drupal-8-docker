05da1c16dae902eb5a99536f96868d5f5090e82a
franhida <thefranhi@hotmail.com>
2020-07-04 14:32:07 +0200

    Changed git credentials location

--- .devcontainer/scripts/first-deploy.sh
+++ .devcontainer/scripts/first-deploy.sh
@@ -3,7 +3,6 @@ cd /var/www/html
 git config --global user.name "$GIT_USER"
 git config --global user.email "$GIT_MAIL"
 git config --global credential.helper store
-echo "https://$GIT_TOKEN@$GIT_DOMAIN" | tee /root/.git-credentials
 git clone https://github.com/franhida/drupal-8-docker.git website
 cd website
 composer install
