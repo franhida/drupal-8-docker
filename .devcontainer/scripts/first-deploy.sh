73ed8841f0398fd754a9380e4ca363029f79d119
franhida <thefranhi@hotmail.com>
2020-07-04 14:30:07 +0200

    Finished Drupal Config

--- .devcontainer/scripts/first-deploy.sh
+++ .devcontainer/scripts/first-deploy.sh
@@ -3,8 +3,8 @@ cd /var/www/html
 git config --global user.name "$GIT_USER"
 git config --global user.email "$GIT_MAIL"
 git config --global credential.helper store
-echo "https://$GIT_TOKEN@$GIT_DOMAIN" > /root/.git-credentials
+echo "https://$GIT_TOKEN@$GIT_DOMAIN" | tee /root/.git-credentials
 git clone https://github.com/franhida/drupal-8-docker.git website
-cd !!:2
+cd website
 composer install
 cd ..
