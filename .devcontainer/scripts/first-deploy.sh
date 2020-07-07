e2e625aa3c08093b298f797bdc101ae426b82676
franhida <thefranhi@hotmil.com>
2020-07-07 17:31:27 +0000

    add files dir and symlink

--- .devcontainer/scripts/first-deploy.sh
+++ .devcontainer/scripts/first-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /var/www/html
+mkdir -p shared/files
 git config --global user.name "$GIT_USER"
 git config --global user.email "$GIT_MAIL"
 git config --global credential.helper store
@@ -7,3 +8,5 @@ git clone https://github.com/franhida/drupal-8-docker.git website
 cd website
 composer install
 cd ..
+mkdir -p $(pwd)/website/web/sites/default
+ln -s $(pwd)/shared/files $(pwd)/website/web/sites/default/files
