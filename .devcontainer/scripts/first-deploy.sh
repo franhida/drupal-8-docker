e21e8779a549933e301127fd1b98d524a6f471ad
franhida <thefranhi@hotmail.com>
2020-07-07 19:55:42 +0200

    update script

--- .devcontainer/scripts/first-deploy.sh
+++ .devcontainer/scripts/first-deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-cd /var/www/html
 mkdir -p /var/www/html/shared/files
 git config --global user.name "$GIT_USER"
 git config --global user.email "$GIT_MAIL"
 git config --global credential.helper store
-git clone https://github.com/franhida/drupal-8-docker.git website
-cd website
+git clone https://github.com/franhida/drupal-8-docker.git /var/www/html/website
+cd !!:2
 composer install
-cd ..
 mkdir -p $(pwd)/website/web/sites/default
 ln -s $(pwd)/shared/files $(pwd)/website/web/sites/default/files
