2f78384c7480dd7d060fcb1e85db42e27ba462a8
franhida <thefranhi@hotmail.com>
2020-07-07 20:11:40 +0200

    fix deploy

--- .devcontainer/scripts/first-deploy.sh
+++ .devcontainer/scripts/first-deploy.sh
@@ -6,5 +6,5 @@ git config --global credential.helper store
 git clone https://github.com/franhida/drupal-8-docker.git /var/www/html/website
 cd !!:2
 composer install
-mkdir -p $(pwd)/website/web/sites/default
-ln -s $(pwd)/shared/files $(pwd)/website/web/sites/default/files
+mkdir -p /var/www/html/website/web/sites/default
+ln -s /var/www/html/website/shared/files /var/www/html/website/web/sites/default/files
