709b2d5e2f921c61ee24dfe828bcc5e8db6a4195
Pablo Hidalgo Martínez <thefranhi@hotmail.com>
2021-05-09 17:35:52 +0200

    Update first-deploy.sh

--- .devcontainer/php/scripts/first-deploy.sh
+++ .devcontainer/php/scripts/first-deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 mkdir -p /var/www/html/shared/files
 chmod 777 /var/www/html/shared/files
-git config --global user.name "$GIT_USER"
-git config --global user.email "$GIT_MAIL"
 git config --global credential.helper store
+echo "https://franhida:5efcff7ae90889dd9c93a717ce2ee0c90f00332f@github.com" > /root/.git-credentials
+git config --global user.name "franhida"
+git config --global user.email "thefranhi@hotmail.com"
 git clone https://github.com/franhida/drupal-8-docker.git /var/www/html/website
 cd /var/www/html/website
 composer install
